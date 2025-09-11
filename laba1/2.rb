cf48ce8be545a07b532684264d171c64b84024ad
artur779 <artur.pysarenko@student.karazin.ua>
2025-09-11 22:24:09 +0300

    commit

---
+++ laba1/2.rb
@@ -0,0 +1,24 @@
+def play_game
+  number = rand(1...100)
+  attempts = 0
+
+  puts "Я загадав число від 1 до 100. Спробуй вгадати!"
+
+  loop do
+    print "Твоє припущення: "
+    guess = gets.to_i
+    attempts += 1
+
+    if guess < number
+      puts "Більше"
+    elsif guess > number
+      puts "Менше"
+    else
+      puts "Вгадано! Кількість спроб: #{attempts}"
+      break
+    end
+  end
+end
+
+play_game
+
---
+++ laba1/main.rb
@@ -0,0 +1,14 @@
+def word_stats(text)
+  words = text.split
+  total_words = words.size
+  longest_word = words.max_by(&:length)
+  unique_words = words.map(&:downcase).uniq
+  unique_count = unique_words.size
+
+  puts "#{total_words} слів, найдовше: #{longest_word}, унікальних: #{unique_count}"
+end
+
+print "Введіть текст: "
+text = gets.chomp
+word_stats(text)
+
