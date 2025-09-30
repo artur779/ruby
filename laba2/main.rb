5c4234da65b90856189953758f20724ef633abc7
artur779 <artur.pysarenko@student.karazin.ua>
2025-09-30 15:26:19 +0300

    commit

--- laba2/main.rb
+++ laba2/main.rb
@@ -1,12 +1,12 @@
 def cut_cake(cake)
   rows = cake.size
   cols = cake.first.size
-  raisins = []
 
   unless cake.all? { |row| row.size == cols }
     raise "Усі рядки мають бути однакової довжини."
   end
 
+  raisins = []
   cake.each_with_index do |row, i|
     row.chars.each_with_index do |cell, j|
       raisins << [i, j] if cell == 'o'
@@ -14,16 +14,21 @@ def cut_cake(cake)
   end
 
   n = raisins.size
-  total_area = rows * cols
-  piece_area = total_area / n
+  raise "Кількість родзинок має бути >1 та <10" unless (2..9).include?(n)
 
+  total_area = rows * cols
   raise "Пиріг не можна поділити рівно!" unless total_area % n == 0
 
-  result = []
-  used = Array.new(rows) { Array.new(cols, false) }
+  piece_area = total_area / n
+  all_results = []
 
-  raisins.each do |(ri, rj)|
-    found = false
+  search = lambda do |remaining_raisins, used, current_pieces|
+    if remaining_raisins.empty?
+      all_results << current_pieces.map(&:dup)
+      return
+    end
+
+    ri, rj = remaining_raisins.first
 
     (0..ri).each do |top|
       (ri...rows).each do |bottom|
@@ -31,46 +36,50 @@ def cut_cake(cake)
           (rj...cols).each do |right|
             h = bottom - top + 1
             w = right - left + 1
+            next unless h * w == piece_area
 
-            if h * w == piece_area
-              sub = []
-              count = 0
-              valid = true
+            sub = []
+            count = 0
+            valid = true
 
-              (top..bottom).each do |ii|
-                line = ""
-                (left..right).each do |jj|
-                  valid = false if used[ii][jj]
-                  line << cake[ii][jj]
-                  count += 1 if cake[ii][jj] == 'o'
+            (top..bottom).each do |ii|
+              line = ""
+              (left..right).each do |jj|
+                if used[ii][jj]
+                  valid = false
+                  break
                 end
-                sub << line
+                line << cake[ii][jj]
+                count += 1 if cake[ii][jj] == 'o'
               end
+              sub << line
+            end
 
-              if valid && count == 1
-                (top..bottom).each { |ii| (left..right).each { |jj| used[ii][jj] = true } }
-                result << sub
-                found = true
-                break
-              end
+            if valid && count == 1
+              new_used = Marshal.load(Marshal.dump(used))
+              (top..bottom).each { |ii| (left..right).each { |jj| new_used[ii][jj] = true } }
+              search.call(remaining_raisins.drop(1), new_used, current_pieces + [sub])
             end
           end
-          break if found
         end
-        break if found
       end
-      break if found
     end
   end
 
-  result
+  used = Array.new(rows) { Array.new(cols, false) }
+  search.call(raisins, used, [])
+
+  raise "Розбиття не знайдено" if all_results.empty?
+
+  best = all_results.max_by { |pieces| pieces.first.first.size }
+  best
 end
 
 cake = [
-  "........",
-  "..o.....",
-  "...o....",
-  "........"
+  ".......",
+  "..o....",
+  "...o...",
+  "......."
 ]
 
 res = cut_cake(cake)
