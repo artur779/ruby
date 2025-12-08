331d4766654419d34937e4f14ca76e1e9af21b61
artur779 <artur.pysarenko@student.karazin.ua>
2025-12-08 11:25:55 +0200

    Add files via upload

---
+++ exam/main.rb
@@ -0,0 +1,28 @@
+class RetryExecutor
+  def initialize(retries: 3, delay: 1) #це параметри за замовчуванням,якщо далі нею вказати які вирористовувати
+    @retries = retries
+    @delay = delay
+  end
+
+  def execute
+    attempts = 0
+
+    begin
+      attempts += 1
+      yield
+    rescue => e
+      puts "Помилка: #{e.message}. Спроба #{attempts}/#{@retries}"
+      if attempts < @retries
+        sleep @delay
+        retry
+      end
+    end
+  end
+end
+
+executor = RetryExecutor.new(retries: 3, delay: 2)
+
+executor.execute do
+  puts "Запускаю дію"
+  raise "Помилка"
+end
